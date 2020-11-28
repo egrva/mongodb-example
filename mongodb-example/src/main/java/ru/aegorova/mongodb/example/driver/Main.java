b8552f9fdd06109f47b93619a3c8ff388a8e0b30
egrva <egorowanastena99@gmail.com>
2020-11-28 10:40:10 +0300

    driver

---
+++ mongodb-example/src/main/java/ru/aegorova/mongodb/example/driver/Main.java
@@ -0,0 +1,31 @@
+package ru.aegorova.mongodb.example.driver;
+
+import com.mongodb.client.*;
+import org.bson.Document;
+
+import java.util.Arrays;
+
+import static com.mongodb.client.model.Projections.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MongoClient client = MongoClients.create();
+        MongoDatabase database = client.getDatabase("store");
+        MongoCollection<Document> collection = database.getCollection("store");
+
+        Document searchQuery = new Document();
+
+        searchQuery
+                .append("status", "paid")
+                .append("$or", Arrays.asList(
+                        new Document("items", "apple"),
+                        new Document("items_count", new Document("$lt", 3))));
+
+        FindIterable<Document> resultDocuments = collection.find(searchQuery)
+                .projection(fields(include("status", "items", "items_count"), excludeId()));
+
+        for (Document document : resultDocuments) {
+            System.out.println(document.toJson());
+        }
+    }
+}
